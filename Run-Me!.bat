6f15e0b2f8686ec70e3abaf8cf69c25fc7f160b5
Vedvod <68318644+Vedvod@users.noreply.github.com>
2021-06-14 19:40:41 +1000

    Add files via upload

--- Run-Me!.bat
+++ Run-Me!.bat
@@ -3,6 +3,6 @@ cd  "%CD%\Scripts\" & rem #This changes the working directory to the Scripts fol
 cls & echo Great Bank ATM, A script by Sameer Sabri and Ved Dhiman & PING localhost -n 6 >NUL & rem #intro message\
 :pythonrun
 	cls & python "%CD%\main.py" & pause & rem #This uses the 'python' command to run the script, replicating manually running the script, then pauses after the program ends
-	set/p again="Run the script again?" 
+	set/p again="Run the script again? " 
 	if /i "%again%" == "yes" goto pythonrun
 	echo Thanks for using our script! & PING localhost -n 4 >NUL & rem #Thanks message and pause
