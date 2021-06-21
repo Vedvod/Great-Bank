298dc43d1bc3286e0027eb8c449d82d02b63180f
Vedvod <68318644+Vedvod@users.noreply.github.com>
2021-06-21 10:13:06 +1000

    Add files via upload

--- Scripts/main.py
+++ Scripts/main.py
@@ -767,18 +767,6 @@ def ask(user, transact, amount): #function to ask the user
     if print_receipt in ["y", "yes"]: #if they answer
                                       #positively
 
-        try:
-          user
-        except:
-          user=input("Which user? ")
-        try:
-          number
-        except:
-            number=int(input("What number? "))
-        while number>3 or number<0:
-          number=input("The current value for number, {number} is invalid! Enter a new value.")
-        os.startfile(os.path.dirname(os.path.realpath(__file__))+f"/receipts/{user}/receipt {number}.txt", "print")
-
 
         wipe() #clear screen to
                #make space for
