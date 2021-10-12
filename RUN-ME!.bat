0c65174d80b1e04be9de40cca3f45ac1afb41fd7
Vedvod <68318644+Vedvod@users.noreply.github.com>
2021-10-13 00:09:43 +1100

    Update RUN-ME!.bat

--- RUN-ME!.bat
+++ RUN-ME!.bat
@@ -1,6 +1,6 @@
 ECHO OFF
 cd  "%CD%\Scripts\" & rem #This changes the working directory to the Scripts folder, where the python script is located. This means that the script runs as though run normally.
-cls & echo Great Bank ATM & PING localhost -n 2 >NUL & echo A script by Sameer Sabri and Ved Dhiman & PING localhost -n 3 >NUL & rem #intro message
+cls & echo Great Bank ATM & PING localhost -n 2 >NUL & echo A script by Sameer and Vedvod & PING localhost -n 3 >NUL & rem #intro message
 cls & py -m pip install "%CD%/six-1.16.0-py2.py3-none-any.whl" "%CD%/keyboard-0.13.5-py3-none-any.whl" "%CD%/colorama-0.4.4-py2.py3-none-any.whl" & rem #This installs the module dependencies
 
 cls & py -m pip install "%CD%/Pillow-8.2.0-cp39-cp39-win_amd64.whl" "%CD%/wxPython-4.1.1-cp39-cp39-win_amd64.whl" "%CD%/numpy-1.20.3-cp39-cp39-win_amd64.whl" & rem #This is for 64 bit
